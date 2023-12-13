9d0105686f398a570285ddf7ce406aa704f2638c
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2023-12-13 12:41:50 +0100

    Fixed build warnings

--- apps/frontend/src/lib/client.ts
+++ apps/frontend/src/lib/client.ts
@@ -7,18 +7,24 @@ import * as React from 'react'
 
 const { possibleTypes } = fragmentTypes
 
+// Base cache configuration
 const cacheConfig : InMemoryCacheConfig = {
     possibleTypes,
-    typePolicies: {}
+    typePolicies: {
+        "ContentModelReference": { keyFields: false },
+        "ContentModelReferenceSearch": { keyFields: false }
+    }
 }
 
-// Make sure to update the policies for all IContent based content types
+// Add policies for all IContent based content types
 possibleTypes.IContent.forEach(iContentType => {
     cacheConfig.typePolicies = cacheConfig.typePolicies ?? {}
     cacheConfig.typePolicies[iContentType] = { 
-        keyFields: [],
+        keyFields: false
     }
 })
+
+//@ToDo: Make content cacheable, without assumptions on the query created
 
 export const { getClient : getServerClient } = registerApolloClient(() => createNewClient(undefined, undefined, undefined, cacheConfig))
 export const getAuthorizedServerClient = React.cache((token: string = ForceHmacAuthentication) => createNewClient(undefined, token, undefined, {
