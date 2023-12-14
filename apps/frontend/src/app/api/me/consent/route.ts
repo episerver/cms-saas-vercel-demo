0cf823155253f0457580c8d7c90a0ef407013a2c
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2023-12-14 17:41:45 +0100

    Add Google Analytics
    Add Google Analytics

---
+++ apps/frontend/src/app/api/me/consent/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse, type NextRequest } from 'next/server'
+import * as EnvTools from '@/lib/env'
+
+type ConsentResponse = {
+    allowTracking: boolean
+}
+
+async function handler(req: NextRequest) : Promise<NextResponse<ConsentResponse>>
+{
+    let consent = req.nextUrl.searchParams.get('set')
+    if (consent == null)
+        consent = req.cookies.get('opt_in')?.value ?? null
+    
+    const hasConsent = consent == null ? true : consent.toLowerCase() == "true"
+    const response = NextResponse.json({
+        allowTracking: hasConsent
+    })
+    response.cookies.set('opt_in', hasConsent ? 'true' : 'false')
+    return response
+}
+
+export const GET = handler
+export const runtime = 'edge' // 'nodejs' (default) | 'edge'
+export const dynamic = 'force-dynamic'
+export const dynamicParams = true
+export const fetchCache = 'default-no-store'
--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -2,11 +2,15 @@ import type { Metadata } from 'next/types'
 import { Inter } from 'next/font/google'
 import getCurrentChannel from '@/lib/current-channel'
 import * as EnvTools from '@/lib/env'
+
+// Client side context
+import GlobalProviders from '@components/providers'
+
+// Client side trackers
 import OdpScript from '@components/integrations/server/optimizely-data-patform'
 import RecsScript from '@components/integrations/server/optimizely-content-recs'
 import WebExScript from '@components/integrations/server/optimizely-web-experimentation'
-
-import GlobalProviders from '@components/providers'
+import GoogleAnalytics from '@/components/integrations/server/google-analytics'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import './globals.css'
@@ -53,17 +57,19 @@ export default function RootLayout({ children }: RootLayoutProps)
     const crecs_client = process.env.OPTIMIZELY_CONTENTRECS_CLIENT ?? undefined
     const crecs_delivery = EnvTools.readValueAsInt("OPTIMIZELY_CONTENTRECS_DELIVERY")
     const exp_id = process.env.OPTIMIZELY_WEB_EXPERIMENTATION_PROJECT ?? undefined
+    const ga_id = process.env.GA_TRACKING_ID ?? undefined
 
     return <html>
         <head>
             { odp_id && <OdpScript trackerId={ odp_id } /> }
-            { crecs_client && crecs_delivery && <RecsScript client={ crecs_client } delivery={ crecs_delivery } /> }
             { exp_id && <WebExScript projectId={ exp_id } /> }
         </head>
         <body className={ `${ inter.className } bg-white text-slate-900` }>
             <GlobalProviders>
                 { children }
             </GlobalProviders>
+            { crecs_client && crecs_delivery && <RecsScript client={ crecs_client } delivery={ crecs_delivery } /> }
+            { ga_id && <GoogleAnalytics measurementId={ ga_id } />}
             <SpeedInsights />
         </body>
     </html>
