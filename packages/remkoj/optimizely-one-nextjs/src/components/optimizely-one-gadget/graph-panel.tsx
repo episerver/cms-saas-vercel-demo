5439102297c1796189febd74c30f19791df4561b
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2024-03-14 10:20:32 +0100

    Fixed styles for OptiOne gadget
    Fixed styles for OptiOne gadget

--- packages/remkoj/optimizely-one-nextjs/src/components/optimizely-one-gadget/graph-panel.tsx
+++ packages/remkoj/optimizely-one-nextjs/src/components/optimizely-one-gadget/graph-panel.tsx
@@ -28,21 +28,21 @@ export const GraphPanel : FunctionComponent<GraphPanelProps> = ({ servicePrefix 
         return <p className='oo-m-2 md:oo-m-4 oo-rounded-md oo-bg-amber-200 oo-border oo-border-amber-800 oo-text-amber-800 oo-p-1 md:oo-p-2'>There was an error loading Optimizely Graph information</p>
 
     return <>
-        <dl className='oo-text-sm'>
+        <dl className='oo-text-[14px]'>
             <dt className='oo-font-bold'>Publishing changes to:</dt>
-            <dd>
+            <dd className='oo-pl-2'>
                 <ul>
-                    { (data?.hooks || []).map(hook => <li key={`webhook-${ hook.url }`}>{ hook.method && <span className='oo-text-xs'>[{ hook.method }] </span>}{ hook.url }{ hook.hasQueryParams && <>?...</>}</li>)} 
+                    { (data?.hooks || []).map(hook => <li key={`webhook-${ hook.url }`}>{ hook.method && <span className='oo-text-amber-800'>({ hook.method?.toLowerCase() ?? '' }) </span>}{ hook.url }{ hook.hasQueryParams && <>?...</>}</li>)} 
                 </ul>
             </dd>
-            <dt className='oo-font-bold oo-pt-1'>Exposing content from:</dt>
-            <dd>
+            <dt className='oo-font-bold oo-pt-4'>Exposing content from:</dt>
+            <dd className='oo-pl-2'>
                 <ul>
                     { (data?.sources || []).map(source => <li key={`source-${ source.label}`}>{ updateLabel(source.label) }, available languages: { source.languages.join(", ")}</li>)}
                 </ul>
             </dd>
         </dl>
-        <p className='oo-pt-4 oo-text-sm'><Link className='oo-underline oo-text-blue-800' href="https://cg.optimizely.com/app/portal/" target='_new'>Login to Optimizely Graph</Link></p>
+        <p className='oo-pt-4 oo-text-[14px]'><Link className='oo-underline oo-text-blue-800' href="https://cg.optimizely.com/app/portal/" target='_new'>Login to Optimizely Graph</Link></p>
     </>
 }
 
