736fd78e17fcf2a4525899dd0810ec47e9ce0747
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2024-07-08 12:01:22 +0200

    Updated GraphQL client to support Visual Builder

--- apps/frontend/src/app/preview/page.tsx
+++ apps/frontend/src/app/preview/page.tsx
@@ -2,7 +2,7 @@ import "server-only";
 
 // Base frameworks & components
 import { type PropsWithChildren } from "react";
-import { OnPageEdit } from "@remkoj/optimizely-cms-nextjs";
+import { OnPageEdit, createAuthorizedClient } from "@remkoj/optimizely-cms-nextjs";
 
 // Import libraries & GraphQL
 import { getContentById } from "@gql";
@@ -12,20 +12,21 @@ import { setupFactory } from "@components/factory";
 import RefreshNotice from "@components/refresh-notice";
 
 // Prepare the Edit Mode Layout for page type components
-const PageLayout = (props: PropsWithChildren<{ locale: string }>) => {
-  return (
-    <>
-      {props.children}
-    </>
-  );
-};
+const PageLayout = ({ children }: PropsWithChildren<{ locale: string }>) => { return children };
 
 export default OnPageEdit.createEditPageComponent(setupFactory(), {
-  refreshNotice: RefreshNotice,
-  layout: PageLayout,
+    refreshNotice: RefreshNotice,
+    layout: PageLayout,
 
-  // Casting is needed due to the locale being an enum in the generated types and a string in the generic query used by the loader
-  loader: getContentById as OnPageEdit.Types.GetContentByIdMethod,
+    // Casting is needed due to the locale being an enum in the generated types and a string in the generic query used by the loader
+    loader: getContentById as OnPageEdit.Types.GetContentByIdMethod,
+    clientFactory: (token) => {
+        const previewClient = createAuthorizedClient(token)
+        previewClient.updateFlags({
+            queryCache: false // Disable query caching as this prevents Visual Builder from working correctly
+        }, false)
+        return previewClient
+    }
 });
 
 export const fetchCache = "force-no-store";
