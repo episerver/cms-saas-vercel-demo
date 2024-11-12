692297f70c181ecb1daf184c1d9cae2050c927cc
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2024-11-12 13:49:48 +0100

    Adjusted CardColumn sizing

--- apps/frontend/src/components/cms/nodes/column/CardColumn/index.tsx
+++ apps/frontend/src/components/cms/nodes/column/CardColumn/index.tsx
@@ -5,7 +5,7 @@ import Card from '@/components/shared/card';
 
 export const CardColumn : CmsLayoutComponent<CardColumnProps> = ({ layoutProps, children }) => {
     const outerCssClasses : string = "vb:column vb:column:CardColumn flex-1"
-    const innerCssClasses: string = `flex flex-col prose prose-p:text-[2rem] prose-span:text-[2rem] max-w-full gap-4 lg:gap-8`;
+    const innerCssClasses: string = `flex flex-col max-w-full gap-4 lg:gap-4`;
     const {
         colBackgroundColor = 'blue',
         itemOrderOnSmallScreen = 'normal'
