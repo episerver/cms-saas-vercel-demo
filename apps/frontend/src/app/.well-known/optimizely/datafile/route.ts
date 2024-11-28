c81e03f68e40b3255cf49a9bc7596daf168d1a4b
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2024-11-28 11:17:08 +0100

    Improved datafile handling

--- apps/frontend/src/app/.well-known/optimizely/datafile/route.ts
+++ apps/frontend/src/app/.well-known/optimizely/datafile/route.ts
@@ -1,44 +1,13 @@
 import { type NextRequest, NextResponse } from "next/server"
-import { readConfigFromEnv as readEnv } from "../../../../opti"
+import { updateDatafile } from "../../../../opti"
 
 async function handler(req: NextRequest)
 {
-    const {edgeConfigId, vercelTeam, vercelToken, token, sdkkey} = readEnv()
-
-    const reqToken = req.nextUrl.searchParams.get('token')
-    if (!token || token != reqToken)
-        return new NextResponse("Not authorized", { status: 401 })
-
-    if (!edgeConfigId || !vercelTeam || !vercelToken || !sdkkey)
-        return new NextResponse("Incorrect configuration", { status: 401 })
-
-    const datafile = await fetch(`https://cdn.optimizely.com/datafiles/${ sdkkey }.json`).then(result => result.json())
-
-    const storeResult = await fetch(`https://api.vercel.com/v1/edge-config/${ edgeConfigId }/items?slug=${ vercelTeam }`, {
-        "body": JSON.stringify({
-            items: [{
-                description: "Optimizely FX Datafile",
-                key: "optimizely-fx-data-file",
-                operation: "upsert",
-                value: datafile
-            },{
-                description: "Optimizely FX Datafile",
-                key: "optimizely-fx-enabled",
-                operation: "upsert",
-                value: true
-            }]
-        }),
-        "headers": {
-          "Authorization": `Bearer ${ vercelToken }`,
-          "Content-Type": "application/json"
-        },
-        "method": "PATCH"
-    }).then(result => result.json())
-
-    if (storeResult.status != 'ok')
-        return NextResponse.json(storeResult, { status: 500 })
-
-    return NextResponse.json(storeResult)
+    const token = req.nextUrl.searchParams.get('token')
+    const updateResult = await updateDatafile(token)
+    if (updateResult.status == "ok")
+        return NextResponse.json(updateResult.data ?? "ok")
+    return NextResponse.json({ message: updateResult.message, data: updateResult.data }, { status: updateResult.code })
 }
 
 export const POST = handler
--- apps/frontend/src/opti.ts
+++ apps/frontend/src/opti.ts
@@ -1,7 +1,14 @@
 import { get } from '@vercel/edge-config'
 import { headers } from 'next/headers'
-import { createInstance, type Client  } from '@optimizely/optimizely-sdk/lite'
+import { createInstance, type Client, type OptimizelyUserContext, type UserAttributes  } from '@optimizely/optimizely-sdk/lite'
 
+/**
+ * Read the Optimizely Feature Experimenation and Vercel Edge Config
+ * configuration from the Environment, for the default Vercel 
+ * implementation of Optimizely Feature Experimentation
+ * 
+ * @returns     Configuration data
+ */
 export function readConfigFromEnv() 
 {
     // @ts-ignore: Node-Types may or may not be available
@@ -25,44 +32,104 @@ export function readConfigFromEnv()
  */
 export async function getInstance() : Promise<Client>
 {
-    let config = (await readDataFileFromEdgeConfig()) ?? (await readDataFileFromCDN())
+    const { sdkkey } = readConfigFromEnv()
+    if (!sdkkey)
+        throw new Error("Optimizely Feature Experimentation key not present")
+
+    let datafile = (await readDataFileFromEdgeConfig(sdkkey)) ?? (await readDataFileFromCDN(sdkkey))
+    if (!datafile)
+        throw new Error("Unable to load datafile, check your sdkKey")
     const fx = createInstance({
-        datafile: config,
+        datafile
     })
     if (!fx)
-        throw new Error("Optimizely Feature Experimentation initialization failed")
+        throw new Error("Optimizely Feature Experimentation not created")
     
-    await fx.onReady()
+    const { success, reason } = await fx.onReady()
+    if (!success)
+        throw new Error("Optimizely Feature Experimentation initialization failed: " + (reason??""))
     return fx
 }
 
-export async function getUserContext()
+/**
+ * Generic method to retrieve the user context for the current request
+ * 
+ * @param       attributes      Any additional attributes to be added
+ * @returns     The User Context for Optimizely Feature Experimentation
+ */
+export async function getUserContext(attributes: UserAttributes = {}) : Promise<OptimizelyUserContext | null>
 {
     const fx = await getInstance()
     const headerData = await headers()
     const visitorId = headerData.get('x-visitorid')
     if (!visitorId)
         throw new Error("No visitor identifier provided by your middleware")
-    
-    const fx_ctx = fx.createUserContext(visitorId, {
 
-    })
+    const fullAttributes = {
+        "user-agent": headerData.get('user-agent'),
+        "platform": headerData.get('sec-ch-ua-platform'),
+        "geo-continent": headerData.get('x-vercel-ip-continent'),
+        "geo-country": headerData.get('x-vercel-ip-country'),
+        "geo-country-region": headerData.get('x-vercel-ip-country-region'),
+        "geo-city": headerData.get('x-vercel-ip-city'),
+        "geo-timezone": headerData.get('x-vercel-ip-timezone'),
+        ...attributes
+    }  
+
+    const fx_ctx = fx.createUserContext(visitorId, fullAttributes)
+    fx_ctx?.fetchQualifiedSegments
     //await fx_ctx?.fetchQualifiedSegments()
     return fx_ctx
 }
 
-export default getUserContext
-
-async function readDataFileFromEdgeConfig() : Promise<string | undefined>
+export async function updateDatafile(authToken?: string | null) : Promise<{ status: "ok", data?: any }|{ status: "error", code: number, message: string, data?: any}>
 {
-    return get<string>('optimizely-fx-data-file').catch(() => undefined)
+    const {edgeConfigId, vercelTeam, vercelToken, token, sdkkey} = readConfigFromEnv()
+
+    if (!token || token != authToken)
+        return { status: "error", code: 401, message: "Not authorized"}
+
+    if (!edgeConfigId || !vercelTeam || !vercelToken || !sdkkey)
+        return { status: "error", message: "Incorrect configuration", code: 401 }
+
+    const datafile = await fetch(`https://cdn.optimizely.com/datafiles/${ sdkkey }.json`).then(result => result.ok ? result.json() as object : undefined).catch(() => undefined)
+    if (!datafile)
+        return { status: "error", message: "Error reading datafile", code: 500 }
+
+    const storeResult = await fetch(`https://api.vercel.com/v1/edge-config/${ edgeConfigId }/items?slug=${ vercelTeam }`, {
+        "body": JSON.stringify({
+            items: [{
+                description: "Optimizely FX Datafile",
+                key: "optimizely-fx-"+sdkkey,
+                operation: "upsert",
+                value: datafile
+            }]
+        }),
+        "headers": {
+          "Authorization": `Bearer ${ vercelToken }`,
+          "Content-Type": "application/json"
+        },
+        "method": "PATCH"
+    }).then(result => result.json() as Record<string,any>).catch(() => undefined)
+
+    if (storeResult?.status != 'ok')
+        return {status: "error", message: "Error storing datafile", data: storeResult, code: 500 }
+    
+    return { status: "ok", data: storeResult }
 }
 
-async function readDataFileFromCDN() : Promise<string | undefined>
+export default getUserContext
+
+async function readDataFileFromEdgeConfig(sdkkey: string) : Promise<string | undefined>
 {
-    const { sdkkey } = readConfigFromEnv()
+    console.log("Loading edge config sdkKey",sdkkey)
+    return get<string>("optimizely-fx-"+sdkkey).catch(() => undefined)
+}
+
+async function readDataFileFromCDN(sdkkey: string) : Promise<string | undefined>
+{
+    console.log("Loading cdn sdkKey",sdkkey)
     const response = await fetch(`https://cdn.optimizely.com/datafiles/${ sdkkey }.json`)
-    if (!response.ok)
-        return undefined
+    if (!response.ok) return undefined
     return response.text().catch(() => undefined)
 }
