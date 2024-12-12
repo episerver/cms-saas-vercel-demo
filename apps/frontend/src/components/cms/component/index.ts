1dbd938adb558f3751dcb18f444bfb810f419fce
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2024-12-12 17:13:28 +0100

    Moved all Elements into the Components folder to align with the new type structure

--- apps/frontend/src/components/cms/component/index.ts
+++ apps/frontend/src/components/cms/component/index.ts
@@ -1,13 +1,20 @@
 // Auto generated dictionary
 // @not-modified => When this line is removed, the "force" parameter of the CLI tool is required to overwrite this file
 import { type ComponentTypeDictionary } from "@remkoj/optimizely-cms-react";
+import VideoElementComponent from "./VideoElement";
 import TextBlockComponent from "./TextBlock";
+import TestimonialElementComponent from "./TestimonialElement";
 import QuoteBlockComponent from "./QuoteBlock";
+import ParagraphElementComponent from "./ParagraphElement";
 import OdpEmbedBlockComponent from "./OdpEmbedBlock";
 import OdpEmbedBlockClientComponent from "./OdpEmbedBlock/client";
 import LayoutContainerBlockComponent from "./LayoutContainerBlock";
+import ImageElementComponent from "./ImageElement";
 import HomePageHeroBlockComponent from "./HomePageHeroBlock";
 import HeroBlockComponent from "./HeroBlock";
+import HeadingElementComponent from "./HeadingElement";
+import CTAElementComponent from "./CTAElement";
+import ContentRecsElementComponent from "./ContentRecsElement";
 import ContentRecsBlockComponent from "./ContentRecsBlock";
 import CarouselBlockComponent from "./CarouselBlock";
 import CarouselBlockCarouselBlockComponent from "./CarouselBlock/carousel-block";
@@ -16,16 +23,29 @@ import CardBlockCardBlockComponent from "./CardBlock/card-block";
 import ButtonBlockComponent from "./ButtonBlock";
 import BlogListingBlockComponent from "./BlogListingBlock";
 import BlogListingBlockBlogListingBlockComponent from "./BlogListingBlock/blog-listing-block";
+import ArticleListElementComponent from "./ArticleListElement";
 
 // Build dictionary
 export const ComponentFactory : ComponentTypeDictionary = [
+    { 
+        type: "VideoElement", 
+        component: VideoElementComponent 
+    },
     { 
         type: "TextBlock", 
         component: TextBlockComponent 
     },
     { 
+        type: "TestimonialElement", 
+        component: TestimonialElementComponent 
+    },
+    { 
         type: "QuoteBlock", 
         component: QuoteBlockComponent 
+    },
+    { 
+        type: "ParagraphElement", 
+        component: ParagraphElementComponent 
     },
     { 
         type: "OdpEmbedBlock", 
@@ -40,12 +60,28 @@ export const ComponentFactory : ComponentTypeDictionary = [
         component: LayoutContainerBlockComponent 
     },
     { 
+        type: "ImageElement", 
+        component: ImageElementComponent 
+    },
+    { 
         type: "HomePageHeroBlock", 
         component: HomePageHeroBlockComponent 
     },
     { 
         type: "HeroBlock", 
         component: HeroBlockComponent 
+    },
+    { 
+        type: "HeadingElement", 
+        component: HeadingElementComponent 
+    },
+    { 
+        type: "CTAElement", 
+        component: CTAElementComponent 
+    },
+    { 
+        type: "ContentRecsElement", 
+        component: ContentRecsElementComponent 
     },
     { 
         type: "ContentRecsBlock", 
@@ -78,6 +114,10 @@ export const ComponentFactory : ComponentTypeDictionary = [
     { 
         type: "BlogListingBlock/blog-listing-block", 
         component: BlogListingBlockBlogListingBlockComponent 
+    },
+    { 
+        type: "ArticleListElement", 
+        component: ArticleListElementComponent 
     }
 ];
 
