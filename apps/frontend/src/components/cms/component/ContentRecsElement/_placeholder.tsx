a99dafbee05dc7fa03a0a9e9d2d19b779c75e6e6
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2025-01-23 14:40:19 +0100

    Adjusted rendition of content recs

--- apps/frontend/src/components/cms/component/ContentRecsElement/_placeholder.tsx
+++ apps/frontend/src/components/cms/component/ContentRecsElement/_placeholder.tsx
@@ -3,7 +3,8 @@ import { type FunctionComponent } from "react"
 
 export const PlaceHolder : FunctionComponent = () => {
     return <div className="w-full aspect-[4/5] on-light-grey blur-lg animate-pulse rounded-[2rem]">
-        
+        <div className="m-12 rounded-[2rem] on-independence aspect-[16/9]"></div>
+        <div className="mx-12 on-independence aspect-[6/1] w-3/4"></div>
     </div>
 }
 
--- apps/frontend/src/components/cms/component/ContentRecsElement/index.tsx
+++ apps/frontend/src/components/cms/component/ContentRecsElement/index.tsx
@@ -5,17 +5,29 @@ import { ContentRecsDelivery } from '@remkoj/optimizely-one-nextjs/client'
 import { CmsEditable } from "@remkoj/optimizely-cms-react/rsc";
 import ArticleTemplate from '../../../shared/article_card/recs-template'
 import PlaceHolder from "./_placeholder";
+import { type ReactNode } from "react";
 
 /**
  * Content Recommendations
  * Add a set of content recommendations
  */
-export const ContentRecsElementElement : CmsComponent<ContentRecsElementDataFragment> = async ({ data, contentLink: { key } }) => {
-    return <CmsEditable as="div" cmsId={ key }>
-        <ContentRecsDelivery template={ ArticleTemplate } count={ data.ElementRecommendationCount ?? 3 } apiKey={ data.ElementDeliveryApiKey ?? '' } className="grid grid-cols-1 lg:grid-cols-3 gap-10 relative pb-12" placeholder={ PlaceHolder } />
+export const ContentRecsElementElement : CmsComponent<ContentRecsElementDataFragment> = async ({ data, contentLink: { key }, inEditMode }) => {
+    return <CmsEditable as="div" cmsId={ key } className="w-full">
+        { inEditMode ? <EditView /> : <ContentRecsDelivery template={ ArticleTemplate } count={ data.ElementRecommendationCount ?? 3 } apiKey={ data.ElementDeliveryApiKey ?? '' } className="grid grid-cols-1 lg:grid-cols-3 gap-10 relative pb-12 w-full" placeholder={ PlaceHolder } />}
     </CmsEditable>
 }
 ContentRecsElementElement.displayName = "Content Recommendations (Element/ContentRecsElement)"
 ContentRecsElementElement.getDataFragment = () => ['ContentRecsElementData', ContentRecsElementDataFragmentDoc]
+
+function EditView({ count = 3 }: { count?: number | null})
+{
+    const tiles : Array<ReactNode> = []
+    for (let idx = 0; idx < (count ?? 3); idx++)
+    {
+        const key = `cr-edit-placeholder-${idx}`
+        tiles.push(<PlaceHolder key={key} />)
+    }
+    return <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 relative pb-12 w-full">{ tiles }</div>
+}
 
 export default ContentRecsElementElement
